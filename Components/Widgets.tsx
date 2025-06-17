45ea34ba2bc5c42b3d0b42f3ebc72c26f47d7148
Hephasoft <198107098+Hephasoft@users.noreply.github.com>
2025-06-17 22:46:49 +0200

    Resolved all of the issues so that Vojta can continue his work.

--- Components/Widgets.tsx
+++ Components/Widgets.tsx
@@ -1,28 +1,24 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/16/solid'
 import React from 'react'
-import { TwitterTimelineEmbed } from 'react-twitter-embed'
+import { Tweet } from 'react-tweet';
 
 
 function Widgets() {
   return (
     <div className='px-2 mt-2 flex-col col-span-2 hidden lg:inline'>
-        {/*Search*/}
-        <div className='flex items-center space-x-2.5 bg-gray-400 p-2 
+      {/*Search*/}
+      <div className='flex items-center space-x-2.5 bg-gray-400 p-2 
         rounded-full max-w-fit mt-2'>
-            <MagnifyingGlassIcon className='size-6 text-yellow-200'/>
-  <input
-        type="text"
-        placeholder="Search B"
-        className="flex-1 bg-transparent outline-none text-yellow-200"
-    />
-    </div>
-
-      <TwitterTimelineEmbed
-        sourceType="profile"
-        screenName="SonnySangha"
-        options={{ height: 1000 }}
+        <MagnifyingGlassIcon className='size-6 text-yellow-200' />
+        <input
+          type="text"
+          placeholder="Search B"
+          className="flex-1 bg-transparent outline-none text-yellow-200"
         />
-        </div>
+      </div>
+
+      <Tweet id='1659125302121857025' />
+    </div>
   )
 }
 
